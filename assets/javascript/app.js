885620e71de0647cd05e9345acf2173e14f89181
Sean Cranston <scranston24@Outlook.com>
2017-12-20 20:41:01 -0500

    added

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+var myQuestions = [
+  {
+    question: "What does the S & P stand for, in regards to the S & P 500?",
+    answerList: ["Simple and Proficient", "Standard and Poor", "Sufficient and Proper", "Stocks and Pensions"],
+    answer: 1
+  },{
+    question: "This is a claim to a part of a corporation's assets and earnings?",
+    answerList: ["Bond", "Index Fund", "Stock", "Fiduciary"],
+    answer: 2
+  },{
+    question: "Berkshire Hathaway is an American multinational conglomerate company, which is owned by this famous investor?",
+    answersList: ["Warren Buffet", "Jeff Bezos", "Dave Ramsey", "Jim Cramer"],
+    answer: 0
+  },{
+     question: "This type of mutual fund is a portfolio contstructed to match or track the components of a market index?",
+     answersList: ["Hedge Fund", "Value Fund", "REIT Fund", "Index Fund"],
+     answer:3
+  },{
+    question: "This famous investor created the Vanguard Group?",
+    answersList: ["Edward Johnson", "Charlie Munger", "Benjamin Graham", "John C. Bogle"],
+    answer: 3
+
+  }];
